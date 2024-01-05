4f0cb9e4e95d32bc33ef00f61b1a1da7d9b55d86
goeunkim <ak0150a01@gmail.com>
2024-01-05 10:10:15 +0900

    reservation

--- src/main/java/com/zerok/reserv/MainMenu.java
+++ src/main/java/com/zerok/reserv/MainMenu.java
@@ -1,5 +1,7 @@
 package com.zerok.reserv;
 
+import java.io.IOException;
+
 class MainMenu extends AbstractMenu{
     private static final MainMenu instance = new MainMenu(null);
     private static final String MAIN_MENU_TEXT =
@@ -21,6 +23,9 @@ class MainMenu extends AbstractMenu{
 
     public Menu next(){
         switch (scanner.nextLine()){
+            case "2":
+                checkReservation();
+                return this;
             case "4":
                 if(! checkAdminPassword()){
                     System.out.println(">> 비밀번호가 틀렸습니다.");
@@ -37,5 +42,19 @@ class MainMenu extends AbstractMenu{
     private boolean checkAdminPassword(){
         System.out.println("관리자 비밀번호를 입력하세요 : ");
         return "admin1234".equals(scanner.nextLine());
+    }
+
+    private void checkReservation(){
+        System.out.println("발급번호를 입력하세요: ");
+        try{
+            Reservation r = Reservation.findById(scanner.nextLine());
+            if(r != null){
+                System.out.printf(">> [확인완료] %s\n", r.toString());
+            } else {
+                System.out.println(">> 예매내역이 없습니다.");
+            }
+        } catch (IOException e){
+            System.out.println(">> 파일 입출력에 문제가 생겼습니다.");
+        }
     }
 }
